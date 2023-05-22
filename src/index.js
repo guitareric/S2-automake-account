671c4b5e4d72656770b5a4f64c3e2b97388fba78
Eric Fluckiger <ericfluckiger@yahoo.com>
2023-05-22 16:25:09 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -41,119 +41,121 @@ const csv = require('csvtojson') // Make sure you have this line in order to cal
   await page.waitForTimeout(3000)
 
   for (const user of users) {
-    // check to see if student to member
-    // Student Lab Access
-    if (user['Member Type'] === 'Student Lab to New Member') {
-      // click admin
-      await page.mouse.click(90, 17, { button: 'left' })
-      await page.waitForTimeout(1000)
-      // click pplsearch
-      await page.mouse.click(142, 304, { button: 'left' })
+    if (!user['Badge Printed and S2 Set']) {
+      // check to see if student to member
+      // Student Lab Access
+      if (user['Member Type'] === 'Student Lab to New Member') {
+        // click admin
+        await page.mouse.click(90, 17, { button: 'left' })
+        await page.waitForTimeout(1000)
+        // click pplsearch
+        await page.mouse.click(142, 304, { button: 'left' })
+        await page.waitForTimeout(2000)
+        // get page focus
+        await page.mouse.click(566, 116, { button: 'left' })
+        await page.waitForTimeout(1000)
+        // scroll down
+        await page.mouse.wheel({ deltaY: 200 })
+        console.log('scrolling')
+        await page.waitForTimeout(1000)
+        // click Last Name field
+        await page.mouse.click(692, 352 + yOffset, { button: 'left' })
+        await page.waitForTimeout(1000)
+        await page.keyboard.type(user['Last Name'])
+        // scroll right
+        await page.mouse.wheel({ deltaX: 300 })
+        await page.mouse.click(692, 352 + yOffset, { button: 'left' })
+        await page.waitForTimeout(1000)
+        await page.keyboard.type(user['First Name'])
+        // click search
+        await page.keyboard.press('Enter')
+        await page.waitForTimeout(2000)
+        // click on user
+        await page.mouse.click(283, 210 + yOffset, { button: 'left' })
+        await page.waitForTimeout(2000)
+        // delete user
+        await frame.waitForSelector('#delete')
+        await frame.click('#delete')
+        await page.waitForTimeout(2000)
+        // click confirm
+        await page.mouse.click(645, 109 + yOffset, { button: 'left' })
+        await page.waitForTimeout(1000)
+        console.log('student deleted')
+      }
+      await page.goto('https://155.98.92.189/frameset/')
       await page.waitForTimeout(2000)
-      // get page focus
-      await page.mouse.click(566, 116, { button: 'left' })
-      await page.waitForTimeout(1000)
-      // scroll down
-      await page.mouse.wheel({ deltaY: 200 })
-      console.log('scrolling')
-      await page.waitForTimeout(1000)
-      // click Last Name field
-      await page.mouse.click(692, 352 + yOffset, { button: 'left' })
+      elementHandle = await page.waitForSelector('#mainFrame')
+      frame = await elementHandle.contentFrame()
+      await frame.waitForSelector('#topbaradmin')
+      await frame.click('#topbaradmin')
+      await frame.waitForSelector('#PplAdd')
+      await frame.click('#PplAdd')
       await page.waitForTimeout(2000)
-      await page.keyboard.type(user['Last Name'])
-      // scroll right
-      await page.mouse.wheel({ deltaX: 300 })
-      await page.mouse.click(692, 352 + yOffset, { button: 'left' })
+      elementHandle = await frame.waitForSelector('#innerPageFrame')
       await page.waitForTimeout(1000)
-      await page.keyboard.type(user['First Name'])
-      // click search
+      frame = await elementHandle.contentFrame()
+      await page.waitForTimeout(1000)
+      await frame.waitForSelector('#lastname')
+      await frame.type('#lastname', user['Last Name'])
+      await frame.type('#firstname', user['First Name'])
+      console.log('first name added')
+      await frame.waitForSelector('#expirationdate_date')
+      await frame.type('#expirationdate_date', '10/1/2024 00:00', { delay: 100 })
       await page.keyboard.press('Enter')
-      await page.waitForTimeout(3000)
-      // click on user
-      await page.mouse.click(283, 210 + yOffset, { button: 'left' })
-      await page.waitForTimeout(3200)
-      // delete user
-      await frame.waitForSelector('#delete')
-      await frame.click('#delete')
-      await page.waitForTimeout(2000)
-      // click confirm
-      await page.mouse.click(645, 109 + yOffset, { button: 'left' })
       await page.waitForTimeout(1000)
-      console.log('student deleted')
-    }
-    await page.goto('https://155.98.92.189/frameset/')
-    await page.waitForTimeout(3000)
-    elementHandle = await page.waitForSelector('#mainFrame')
-    frame = await elementHandle.contentFrame()
-    await frame.waitForSelector('#topbaradmin')
-    await frame.click('#topbaradmin')
-    await frame.waitForSelector('#PplAdd')
-    await frame.click('#PplAdd')
-    await page.waitForTimeout(2000)
-    elementHandle = await frame.waitForSelector('#innerPageFrame')
-    await page.waitForTimeout(1000)
-    frame = await elementHandle.contentFrame()
-    await page.waitForTimeout(1000)
-    await frame.waitForSelector('#lastname')
-    await frame.type('#lastname', user['Last Name'])
-    await frame.type('#firstname', user['First Name'])
-    console.log('first name added')
-    await frame.waitForSelector('#expirationdate_date')
-    await frame.type('#expirationdate_date', '10/1/2024 00:00', { delay: 100 })
-    await page.keyboard.press('Enter')
-    await page.waitForTimeout(3000)
-    console.log('expiration date added')
-    await frame.click('#tab-credentialtab')
-    await frame.waitForSelector('#addcredential')
-    await frame.click('#addcredential')
-    await frame.waitForSelector('#encodednumber0')
-    await frame.type('#encodednumber0', user['Prox Code'])
-    await frame.click('#tab-accesstab')
-    console.log('access tab clicked')
+      console.log('expiration date added')
+      await frame.click('#tab-credentialtab')
+      await frame.waitForSelector('#addcredential')
+      await frame.click('#addcredential')
+      await frame.waitForSelector('#encodednumber0')
+      await frame.type('#encodednumber0', user['Prox Code'])
+      await frame.click('#tab-accesstab')
+      console.log('access tab clicked')
 
-    // New Member/PI access levels
-    if (user['Member Type'] === 'New Member' || user['Member Type'] === 'PI') {
-      // backend lab
-      await frame.waitForSelector('#\\35 9 > td:nth-child(3)')
-      await frame.click('#\\35 9 > td:nth-child(3)')
-      // clean conference room
-      await frame.waitForSelector('#\\36 2 > td:nth-child(3)')
-      await frame.click('#\\36 2 > td:nth-child(3)')
-      // cleanroom day only
-      await frame.waitForSelector('#\\36 4 > td:nth-child(3)')
-      await frame.click('#\\36 4 > td:nth-child(3)')
-      // cleanroom exit
-      await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
-      await frame.click('#\\37 7 > td:nth-child(3)')
-      // prototyping lab
-      await frame.waitForSelector('#\\37 0 > td:nth-child(3)')
-      await frame.click('#\\37 0 > td:nth-child(3)')
-    }
+      // New Member/PI access levels
+      if (user['Member Type'] === 'New Member' || user['Member Type'] === 'PI') {
+        // backend lab
+        await frame.waitForSelector('#\\35 9 > td:nth-child(3)')
+        await frame.click('#\\35 9 > td:nth-child(3)')
+        // clean conference room
+        await frame.waitForSelector('#\\36 2 > td:nth-child(3)')
+        await frame.click('#\\36 2 > td:nth-child(3)')
+        // cleanroom day only
+        await frame.waitForSelector('#\\36 4 > td:nth-child(3)')
+        await frame.click('#\\36 4 > td:nth-child(3)')
+        // cleanroom exit
+        await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
+        await frame.click('#\\37 7 > td:nth-child(3)')
+        // prototyping lab
+        await frame.waitForSelector('#\\37 0 > td:nth-child(3)')
+        await frame.click('#\\37 0 > td:nth-child(3)')
+      }
 
-    // Student Lab Access
-    if (user['Member Type'] === 'Student Lab') {
-      await frame.waitForSelector('#\\36 1 > td:nth-child(3)')
-      await frame.click('#\\36 1 > td:nth-child(3)')
-      await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
-      await frame.click('#\\37 7 > td:nth-child(3)')
-    }
+      // Student Lab Access
+      if (user['Member Type'] === 'Student Lab') {
+        await frame.waitForSelector('#\\36 1 > td:nth-child(3)')
+        await frame.click('#\\36 1 > td:nth-child(3)')
+        await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
+        await frame.click('#\\37 7 > td:nth-child(3)')
+      }
 
-    // Staff Lab Access
-    if (user['Member Type'] === 'Student Lab') {
-      await frame.waitForSelector('#\\36 1 > td:nth-child(3)')
-      await frame.click('#\\36 1 > td:nth-child(3)')
-      await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
-      await frame.click('#\\37 7 > td:nth-child(3)')
-    }
+      // Staff Lab Access
+      if (user['Member Type'] === 'Student Lab') {
+        await frame.waitForSelector('#\\36 1 > td:nth-child(3)')
+        await frame.click('#\\36 1 > td:nth-child(3)')
+        await frame.waitForSelector('#\\37 7 > td:nth-child(3)')
+        await frame.click('#\\37 7 > td:nth-child(3)')
+      }
 
-    await page.waitForTimeout(300)
-    await frame.click('#uparrow')
-    await page.waitForTimeout(500)
-    console.log('about to click save')
-    await page.waitForTimeout(500)
-    await frame.click('#save')
-    await page.waitForTimeout(3000)
-    console.log('member added successfully')
+      await page.waitForTimeout(300)
+      await frame.click('#uparrow')
+      await page.waitForTimeout(500)
+      console.log('about to click save')
+      await page.waitForTimeout(500)
+      await frame.click('#save')
+      await page.waitForTimeout(3000)
+      console.log('member added successfully')
+    }
   }
 
   // Charge new member fee
@@ -172,54 +174,58 @@ const csv = require('csvtojson') // Make sure you have this line in order to cal
   await navigationPromise
 
   for (const user of users) {
-    await page.goto('https://resource.cores.utah.edu/#/group/13')
-    await page.waitForSelector('div > div > div:nth-child(2) > div > .bp4-callout:nth-child(4)')
-    await page.click('div > div > div:nth-child(2) > div > .bp4-callout:nth-child(4)')
+    if (!user['Badge Printed and S2 Set']) {
+      await page.goto('https://resource.cores.utah.edu/#/group/13')
+      await page.waitForSelector('div > div > div:nth-child(2) > div > .bp4-callout:nth-child(4)')
+      await page.click('div > div > div:nth-child(2) > div > .bp4-callout:nth-child(4)')
 
-    await page.waitForSelector('div > div:nth-child(16) > .bp4-button > .bp4-icon > svg')
-    await page.click('div > div:nth-child(16) > .bp4-button > .bp4-icon > svg')
+      await page.waitForSelector('div > div:nth-child(16) > .bp4-button > .bp4-icon > svg')
+      await page.click('div > div:nth-child(16) > .bp4-button > .bp4-icon > svg')
 
-    await page.waitForSelector('div:nth-child(1) > div > div:nth-child(2) > .bp4-button > .bp4-icon-caret-down > svg')
-    await page.click('div:nth-child(1) > div > div:nth-child(2) > .bp4-button > .bp4-icon-caret-down > svg')
+      await page.waitForSelector('div:nth-child(1) > div > div:nth-child(2) > .bp4-button > .bp4-icon-caret-down > svg')
+      await page.click('div:nth-child(1) > div > div:nth-child(2) > .bp4-button > .bp4-icon-caret-down > svg')
 
-    await page.waitForSelector('.bp4-collapse > .bp4-collapse-body > div > div > .bp4-callout:nth-child(2)')
-    await page.click('.bp4-collapse > .bp4-collapse-body > div > div > .bp4-callout:nth-child(2)')
+      await page.waitForSelector(
+        '#content > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(16) > div > div > div > div > div:nth-child(2) > div > div > div > div > div:nth-child(2) > span'
+      )
+      await page.click('#content > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(16) > div > div > div > div > div:nth-child(2) > div > div > div > div > div:nth-child(2) > span')
 
-    await page.waitForSelector('#content > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div > div > span:nth-child(3) > button > span > svg')
-    await page.click('#content > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div > div > span:nth-child(3) > button > span > svg')
+      await page.waitForSelector('#content > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div > div > span:nth-child(3) > button > span > svg')
+      await page.click('#content > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div > div > span:nth-child(3) > button > span > svg')
 
-    await page.waitForSelector(
-      'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input'
-    )
-    await page.click(
-      'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input'
-    )
+      await page.waitForSelector(
+        'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input'
+      )
+      await page.click(
+        'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input'
+      )
 
-    await page.type(
-      'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input',
-      user['First Name'] + ' ' + user['Last Name']
-    )
+      await page.type(
+        'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > div.bp4-input-group.bp4-fill > input',
+        user['First Name'] + ' ' + user['Last Name']
+      )
 
-    await page.waitForSelector(
-      'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(1) > button > span'
-    )
-    await page.click(
-      'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(1) > button > span'
-    )
+      await page.waitForSelector(
+        'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(1) > button > span'
+      )
+      await page.click(
+        'body > div.bp4-portal > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(1) > button > span'
+      )
 
-    await page.waitForSelector(
-      'body > div:nth-child(12) > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(1) > button > span'
-    )
-    await page.click(
-      'body > div:nth-child(12) > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(1) > button > span'
-    )
+      await page.waitForSelector(
+        'body > div:nth-child(12) > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(1) > button > span'
+      )
+      await page.click(
+        'body > div:nth-child(12) > div > div.bp4-popover2-transition-container.bp4-overlay-content.bp4-popover2-appear-done.bp4-popover2-enter-done > div > div.bp4-popover2-content > div > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(1) > button > span'
+      )
 
-    await page.type('#content > div:nth-child(2) > div > div > div:nth-child(4) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div.bp4-input-group > input', '1')
-    // press submit button
-    await page.waitForSelector('#content > div:nth-child(2) > div > div > div:nth-child(6) > button.bp4-button.bp4-intent-success')
-    await page.click('#content > div:nth-child(2) > div > div > div:nth-child(6) > button.bp4-button.bp4-intent-success')
+      await page.type('#content > div:nth-child(2) > div > div > div:nth-child(4) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div.bp4-input-group > input', '1')
+      // press submit button
+      await page.waitForSelector('#content > div:nth-child(2) > div > div > div:nth-child(6) > button.bp4-button.bp4-intent-success')
+      await page.click('#content > div:nth-child(2) > div > div > div:nth-child(6) > button.bp4-button.bp4-intent-success')
 
-    // end loop
+      // end loop
+    }
   }
   await browser.close()
   console.log('all members added successfully')
